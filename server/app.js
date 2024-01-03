f401331235e9b6bc497f6a9498a2e0ec89d8c29d
Gustavo Perin <gustavoperin067@gmail.com>
2024-01-03 18:45:42 -0300

    Changing api routing

--- server/app.js
+++ server/app.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const app = express();
+const apiRouter = express.Router();
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/api", apiRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-app.get("/register", (req, res) => {
+apiRouter.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+apiRouter.get("/register", (req, res) => {
   // Handle registration logic here
   res.send("Registration endpoint");
 });
 
-app.get("/login", (req, res) => {
+apiRouter.get("/login", (req, res) => {
   // Handle login logic here
   res.send("Login endpoint");
 });
