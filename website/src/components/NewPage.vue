b6b7bb23d002bc5ed1980464ac3de4ca34d9bf9a
Gustavo Perin <gustavoperin067@gmail.com>
2024-01-03 04:03:20 -0300

    Adding download button

--- website/src/components/NewPage.vue
+++ website/src/components/NewPage.vue
@@ -2,6 +2,13 @@
   <div>
     <h1>New Page</h1>
     <p>This is the new page.</p>
+    <!-- Download Button -->
+    <a
+      href="https://bobagi.net/downloads/dist.7z"
+      download
+      class="download-button"
+      >Download</a
+    >
   </div>
 </template>
 
@@ -10,3 +17,17 @@ export default {
   name: "NewPage",
 };
 </script>
+
+<style>
+.download-button {
+  padding: 10px 20px;
+  background-color: #4caf50;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.download-button:hover {
+  background-color: #45a049;
+}
+</style>
