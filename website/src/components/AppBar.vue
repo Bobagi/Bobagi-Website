79b23704b452a7088b4f0069b7228e85ecfbc16f
Gustavo Perin <gustavoperin067@gmail.com>
2024-03-03 18:06:45 -0300

    Adding height 100% to appBar buttons

--- website/src/components/AppBar.vue
+++ website/src/components/AppBar.vue
@@ -1,92 +1,96 @@
 <template>
   <v-app-bar>
     <v-toolbar color="background">
-      <v-toolbar-items>
-        <v-btn text color="primary" to="/">Home</v-btn>
-        <v-menu>
-          <template v-slot:activator="{ props }">
-            <v-btn color="primary" v-bind="props"> Projects</v-btn>
-          </template>
-          <v-list>
-            <v-list-item link to="/HeroWars">
-              <v-list-item-title class="tertiary-color"
-                >HeroWars</v-list-item-title
-              >
-            </v-list-item>
-            <v-list-item link to="/ProjectZomboid">
-              <v-list-item-title class="tertiary-color"
-                >Zomboid</v-list-item-title
-              >
-            </v-list-item>
-            <v-list-item link to="/SnowFlake">
-              <v-list-item-title class="tertiary-color"
-                >Snowflake</v-list-item-title
-              >
-            </v-list-item>
-            <v-list-item link to="/Avarice">
-              <v-list-item-title class="tertiary-color"
-                >Avarice</v-list-item-title
-              >
-            </v-list-item>
-            <v-list-item link to="/CoinAlert">
-              <v-list-item-title class="tertiary-color"
-                >Coin Alert</v-list-item-title
-              >
-            </v-list-item>
-          </v-list>
-        </v-menu>
-        <v-menu>
-          <template v-slot:activator="{ props }">
-            <v-btn color="primary" v-bind="props">Games</v-btn>
-          </template>
-          <v-list>
-            <v-list-item link to="/TicTacToe">
-              <v-list-item-title class="tertiary-color"
-                >Tic Tac Toe</v-list-item-title
-              >
-            </v-list-item>
-            <v-list-item link to="/GoldRush">
-              <v-list-item-title class="tertiary-color"
-                >Goldrush Survivors</v-list-item-title
-              >
-            </v-list-item>
-            <v-list-item link to="/OneWayFly">
-              <v-list-item-title class="tertiary-color"
-                >One Way Fly</v-list-item-title
-              >
-            </v-list-item>
-          </v-list>
-        </v-menu>
+      <v-btn text height="100%" color="primary" to="/">Home</v-btn>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn color="primary" height="100%" v-bind="props"> Projects</v-btn>
+        </template>
+        <v-list>
+          <v-list-item link to="/HeroWars">
+            <v-list-item-title class="tertiary-color"
+              >HeroWars</v-list-item-title
+            >
+          </v-list-item>
+          <v-list-item link to="/ProjectZomboid">
+            <v-list-item-title class="tertiary-color"
+              >Zomboid</v-list-item-title
+            >
+          </v-list-item>
+          <v-list-item link to="/SnowFlake">
+            <v-list-item-title class="tertiary-color"
+              >Snowflake</v-list-item-title
+            >
+          </v-list-item>
+          <v-list-item link to="/Avarice">
+            <v-list-item-title class="tertiary-color"
+              >Avarice</v-list-item-title
+            >
+          </v-list-item>
+          <v-list-item link to="/CoinAlert">
+            <v-list-item-title class="tertiary-color"
+              >Coin Alert</v-list-item-title
+            >
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn color="primary" height="100%" v-bind="props">Games</v-btn>
+        </template>
+        <v-list>
+          <v-list-item link to="/TicTacToe">
+            <v-list-item-title class="tertiary-color"
+              >Tic Tac Toe</v-list-item-title
+            >
+          </v-list-item>
+          <v-list-item link to="/GoldRush">
+            <v-list-item-title class="tertiary-color"
+              >Goldrush Survivors</v-list-item-title
+            >
+          </v-list-item>
+          <v-list-item link to="/OneWayFly">
+            <v-list-item-title class="tertiary-color"
+              >One Way Fly</v-list-item-title
+            >
+          </v-list-item>
+        </v-list>
+      </v-menu>
 
-        <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-        <v-btn icon @click="toggleIcon">
-          <v-icon color="primary">{{
-            isDark ? "mdi-weather-night" : "mdi-weather-sunny"
-          }}</v-icon>
-        </v-btn>
+      <v-btn icon @click="toggleIcon">
+        <v-icon color="primary">{{
+          isDark ? "mdi-weather-night" : "mdi-weather-sunny"
+        }}</v-icon>
+      </v-btn>
 
-        <!-- Show Sign In and Sign Up only if no user is logged in -->
-        <v-btn v-if="!user" text color="primary" to="/SignIn">Sign In</v-btn>
-        <v-btn v-if="!user" text color="primary" to="/SignUp">Sign Up</v-btn>
+      <!-- Show Sign In and Sign Up only if no user is logged in -->
+      <v-btn v-if="!user" text height="100%" color="primary" to="/SignIn"
+        >Sign In</v-btn
+      >
+      <v-btn v-if="!user" text height="100%" color="primary" to="/SignUp"
+        >Sign Up</v-btn
+      >
 
-        <v-btn
-          v-if="user"
-          color="primary"
-          icon="mdi-cog-outline"
-          to="/UserConfig"
-        ></v-btn>
+      <v-btn
+        v-if="user"
+        color="primary"
+        height="100%"
+        icon="mdi-cog-outline"
+        to="/UserConfig"
+      ></v-btn>
 
-        <!-- Show Disconnect if a user is logged in -->
-        <v-btn
-          v-if="user"
-          text
-          color="primary"
-          prepend-icon="mdi-logout"
-          @click="disconnectUser"
-          >Disconnect</v-btn
-        >
-      </v-toolbar-items>
+      <!-- Show Disconnect if a user is logged in -->
+      <v-btn
+        v-if="user"
+        text
+        height="100%"
+        color="primary"
+        prepend-icon="mdi-logout"
+        @click="disconnectUser"
+        >Disconnect</v-btn
+      >
     </v-toolbar>
   </v-app-bar>
 </template>
