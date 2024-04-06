a5e5617d6a8eb85ea2671f38c423d380eeed3989
Gustavo Perin <gustavoperin067@gmail.com>
2024-04-05 22:23:23 -0300

    style: Formatting Home Page content

--- website/src/components/HomePage.vue
+++ website/src/components/HomePage.vue
@@ -23,38 +23,47 @@
           class="textContent"
           style="word-wrap: break-word; text-align: left"
         >
-          <v-row
-            >Hello 👋<br />
-            My name is Gustavo, and i'm a Brazilian programmer. I like to
+          <v-row class="mb-3">Hello 👋 </v-row>
+          <v-row class="mb-3">
+            My name is Gustavo, and I'm a Brazilian programmer. I like to
             explore different areas of development and figure out the next steps
             in life.
-            <br /><br />
+          </v-row>
+
+          <v-row class="mb-3">
             🌱 Currently, I am focusing on learning Python, Linux, and Vue.
-            <br /><br />
-            Buy me a coffee ☕❤️
+          </v-row>
+
+          <v-row>Buy me a coffee ☕❤️</v-row>
+          <v-row class="mb-3">
             <a
               href="https://www.paypal.com/donate?business=gustavoperin067%40gmail.com&item_name=Support+Gustavo's+Journey&currency_code=USD"
               target="_blank"
-              >Donate with PayPal</a
             >
-            <br /><br />
-            Contact 📫:<br />
-            Feedback, ideas, or anything else you'd like to share... send me an
-            email 😄
-            <a href="mailto:gustavoperin067@gmail.com"
-              >gustavoperin067@gmail.com</a
-            ><br /><span class="betweenLines"
-              >(The answer may take a bit to happen... maybe never, but have
-              faith.)</span
-            >
+              Donate with PayPal
+            </a>
           </v-row>
 
-          <v-row class="d-flex align-center" style="justify-content: center">
+          <v-row>Contact 📫:</v-row>
+          <v-row>
+            Feedback, ideas, or anything else you'd like to share... send me an
+            email 😄
+          </v-row>
+          <v-row class="mb-3">
+            <a href="mailto:gustavoperin067@gmail.com"
+              >gustavoperin067@gmail.com</a
+            >
+          </v-row>
+          <v-row class="mb-3">
+            <span class="betweenLines">
+              (The answer may take a bit to happen... maybe never, but have
+              faith.)
+            </span>
+          </v-row>
+
+          <v-row class="d-flex align-center justify-center mb-3">
             <v-col>
-              <div
-                class="d-flex flex-wrap ga-3 justify-center"
-                justify="center"
-              >
+              <div class="d-flex flex-wrap ga-3 justify-center">
                 <v-btn
                   color="primary"
                   size="large"
@@ -73,7 +82,7 @@
                   target="_blank"
                 >
                   <v-icon icon="mdi-linkedin" size="large" start></v-icon>
-                  Linkdin
+                  LinkedIn
                 </v-btn>
               </div>
             </v-col>
